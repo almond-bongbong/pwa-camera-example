e7125c75d45e7de7bda9d4a5c33d83123bb37a9f
cmlee <bal.dongdongdong@gmail.com>
2020-07-17 01:04:32 +0900

    initalize

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <header className="App-header">
+        hello
+      </header>
+    </div>
+  );
+}
+
+export default App;
