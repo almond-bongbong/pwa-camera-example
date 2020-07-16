7546c954b293cd6229487e8f36c62970c0472229
cmlee <bal.dongdongdong@gmail.com>
2020-07-17 01:42:46 +0900

    add camera, upload feature

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import styles from './App.module.scss';
 
 function App() {
+  const [preview, setPreview] = useState('');
+
+  const upload = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.files);
+    const { files } = e.target;
+
+    if (files && files.length > 0) {
+      const url = window.URL.createObjectURL(files[0]);
+      const image = new Image();
+      image.src = url;
+      image.onload = () => {
+        setPreview(image.src);
+        window.URL.revokeObjectURL(image.src);
+      };
+    }
+  };
+
   return (
     <div className={styles.app}>
-      <header className="App-header">
-        hello
-      </header>
+      <div className={styles.camera}>
+        <input
+          id="take-photo"
+          type="file"
+          accept="image/*;capture=camera"
+          onChange={upload}
+        />
+        <label htmlFor="take-photo">Take Photo</label>
+      </div>
+
+      <div className={styles.camera}>
+        <input
+          id="upload"
+          type="file"
+          accept="image/x-png,image/jpeg,image/gif"
+          multiple={false}
+          onChange={upload}
+        />
+        <label htmlFor="upload">Upload Photo</label>
+      </div>
+
+      {preview && (
+        <img className={styles.preview} src={preview} alt="preview" />
+      )}
     </div>
   );
 }
