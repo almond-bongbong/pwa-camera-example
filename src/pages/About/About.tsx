43f70fe748ed4cd45d5276e7a326e13425fcaffc
cmlee <bal.dongdongdong@gmail.com>
2020-08-22 20:43:12 +0900

    update path

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 function About() {
-  return <div>Hello About</div>;
+  return (
+    <div>
+      <Helmet>
+        <link rel="manifest" href={`${process.env.PUBLIC_URL}/about-manifest.json`} />
+      </Helmet>
+      <span>Hello About</span>
+    </div>
+  );
 }
 
 export default About;
